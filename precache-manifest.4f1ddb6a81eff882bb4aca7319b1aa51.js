f3a1e759a3498c1f3bb28c3c8c93cab5e94b1e13
Oles Marola <oles_pw@ukr.net>
2019-12-10 19:48:57 +0200

    Updates

---
+++ precache-manifest.4f1ddb6a81eff882bb4aca7319b1aa51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4774654ae310ad405c0e827a362d22a3",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "e785eba791e51e31cc6a",
+    "url": "/react_add-todo-form/static/css/main.c66c4450.chunk.css"
+  },
+  {
+    "revision": "65d10dde00e195b8ec90",
+    "url": "/react_add-todo-form/static/js/2.ea31de94.chunk.js"
+  },
+  {
+    "revision": "e785eba791e51e31cc6a",
+    "url": "/react_add-todo-form/static/js/main.50c5d5e3.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
